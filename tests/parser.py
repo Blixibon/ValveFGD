e2f5a7a78e1897b52c8eb60a3be5fd6a28e5a959
maxdup <mdupuis@hotmail.ca>
2022-11-07 22:21:44 -0500

    improved test methodology

--- tests/parser.py
+++ tests/parser.py
@@ -14,106 +14,138 @@ class ParseTestCase(unittest.TestCase):
     def test_bms(self):
         fgd = FgdParse('tests/fgds/bms/bms.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(829, len(fgd.entities))
 
     def test_cs(self):
         fgd = FgdParse('tests/fgds/cs/halflife-cs.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(100, len(fgd.entities))
 
     def test_csgo(self):
         fgd = FgdParse('tests/fgds/csgo/csgo.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(460, len(fgd.entities))
 
     def test_cstrike(self):
         fgd = FgdParse('tests/fgds/css/cstrike.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(356, len(fgd.entities))
 
     def test_dod(self):
         fgd = FgdParse('tests/fgds/dod/halflife-DOD2-expert.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(129, len(fgd.entities))
 
     def test_dods(self):
         fgd = FgdParse('tests/fgds/dods/dod.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(315, len(fgd.entities))
 
     def test_ep1(self):
         fgd = FgdParse('tests/fgds/ep1/cstrike.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(352, len(fgd.entities))
         fgd = FgdParse('tests/fgds/ep1/dod.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(310, len(fgd.entities))
         fgd = FgdParse('tests/fgds/ep1/halflife2.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(352, len(fgd.entities))
         fgd = FgdParse('tests/fgds/ep1/hl2mp.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(358, len(fgd.entities))
         fgd = FgdParse('tests/fgds/ep1/portal.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(374, len(fgd.entities))
         fgd = FgdParse('tests/fgds/ep1/sdk.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(5, len(fgd.entities))
         fgd = FgdParse('tests/fgds/ep1/tf.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(328, len(fgd.entities))
 
     def test_garrysmod(self):
         fgd = FgdParse('tests/fgds/garrysmod/garrysmod.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(659, len(fgd.entities))
 
     def test_hl(self):
         fgd = FgdParse('tests/fgds/hl/halflife.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(217, len(fgd.entities))
 
     def test_hldc(self):
         fgd = FgdParse('tests/fgds/hldc/dmc.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(137, len(fgd.entities))
 
     def test_hl2(self):
         fgd = FgdParse('tests/fgds/hl2/halflife2.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(354, len(fgd.entities))
 
     def test_left4dead(self):
         fgd = FgdParse('tests/fgds/left4dead/left4dead.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(379, len(fgd.entities))
 
     def test_left4dead2(self):
         fgd = FgdParse('tests/fgds/left4dead2/left4dead2.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(437, len(fgd.entities))
 
     def test_orangebox(self):
         fgd = FgdParse('tests/fgds/orangebox/cstrike.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(354, len(fgd.entities))
         fgd = FgdParse('tests/fgds/orangebox/dod.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(313, len(fgd.entities))
         fgd = FgdParse('tests/fgds/orangebox/halflife2.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(354, len(fgd.entities))
         fgd = FgdParse('tests/fgds/orangebox/hl2mp.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(360, len(fgd.entities))
         fgd = FgdParse('tests/fgds/orangebox/portal.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(376, len(fgd.entities))
         fgd = FgdParse('tests/fgds/orangebox/sdk.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(13, len(fgd.entities))
         fgd = FgdParse('tests/fgds/orangebox/tf.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(378, len(fgd.entities))
 
     def test_portal(self):
         fgd = FgdParse('tests/fgds/portal/portal.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(376, len(fgd.entities))
 
     def test_portal2(self):
         fgd = FgdParse('tests/fgds/portal2/portal2.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(1122, len(fgd.entities))
 
     def test_ricochet(self):
         fgd = FgdParse('tests/fgds/ricochet/ricochet.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(10, len(fgd.entities))
 
     def test_swarm(self):
         fgd = FgdParse('tests/fgds/swarm/swarm.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(389, len(fgd.entities))
 
     def test_tf(self):
         fgd = FgdParse('tests/fgds/tf/tf.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(442, len(fgd.entities))
 
     def test_tfc(self):
         fgd = FgdParse('tests/fgds/tfc/tf15f.fgd')
         self.assertTrue(fgd)
+        self.assertEqual(52, len(fgd.entities))
 
     def test_notfound(self):
         self.assertRaises(IOError, FgdParse,
